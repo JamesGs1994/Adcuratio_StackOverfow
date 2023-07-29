e5bf5efbb06607c9227a56fe90539022e713a3fe
James Gs <81745865+JamesGs1994@users.noreply.github.com>
2023-07-29 11:24:30 +0530

    Update ReactNativeScreen.js

--- App/Screens/ReactNativeScreen.js
+++ App/Screens/ReactNativeScreen.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import AdcView from '../commonComponents/AdcView';
 import AdcCallBackAPI from '../commonComponents/AdcCallBackAPI';
+import AdcText from '../commonComponents/AdcText';
 
-const ReactNativeScreen = () => {
+const ReactScreen = () => {
   return (
     <AdcView centerAlign flex1>
+      <AdcView row sPaddingBottom paddingTop borderLine>
+        <AdcText topQuestion>{`Top Questions`}</AdcText>
+      </AdcView>
       <AdcCallBackAPI topic={'react'} />
     </AdcView>
   );
 };
 
-export default ReactNativeScreen;
+export default ReactScreen;
