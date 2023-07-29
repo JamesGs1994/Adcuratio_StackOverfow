b9a1de771eebe301ed119769a54f3822a10d24a7
James Gs <81745865+JamesGs1994@users.noreply.github.com>
2023-07-29 11:24:53 +0530

    Update NodeScreen.js

--- App/Screens/NodeScreen.js
+++ App/Screens/NodeScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AdcView from '../commonComponents/AdcView';
 import AdcCallBackAPI from '../commonComponents/AdcCallBackAPI';
+import AdcText from '../commonComponents/AdcText';
 
 const NodeScreen = () => {
   return (
     <AdcView centerAlign flex1>
+      <AdcView row sPaddingBottom paddingTop borderLine>
+        <AdcText topQuestion>{`Top Questions`}</AdcText>
+      </AdcView>
       <AdcCallBackAPI topic={'nodejs'} />
     </AdcView>
   );
